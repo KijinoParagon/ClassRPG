e4832e55bc3db94553288b74448ae766a0f7f157
gnarlsley <thompson.garrett7413@gmail.com>
2023-09-06 13:25:01 -0500

    Ya

---
+++ ClassRPG/Controllers/GarrettController.cs
@@ -0,0 +1,12 @@
+﻿using ClassRPG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassRPG.Controllers
+{
+    public class GarrettController : Controller
+    {
+        public IActionResult Index() { return View(); }
+
+        public IActionResult Display(GarrettModel model) { return View(model); }
+    }
+}
