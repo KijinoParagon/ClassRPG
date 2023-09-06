437cd00fe9decc5a4716586ef43512127a1c4146
JKirkis <jordkerkvliet@gmail.com>
2023-09-06 14:02:40 -0500

    Jordan Page update

---
+++ ClassRPG/Models/JordanModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassRPG.Models
+{
+    public class JordanModel
+    {
+        public string? Name { get; set; }
+        public string? Color { get; set; }
+        public Class CharacterClass { get; set; }
+    }
+
+    public enum Class
+    {
+        Warrior,
+        Rogue,
+        Priest
+    }
+}
